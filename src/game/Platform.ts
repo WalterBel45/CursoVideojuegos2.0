f0caa1859c58a8b84fa349527db1afde9f966fb6
Walter Bel <arielbel45@gmail.com>
2024-05-26 14:23:46 -0300

    Cambio en la probabilidad en que aparecen las posciones

--- src/game/Platform.ts
+++ src/game/Platform.ts
@@ -1,19 +1,20 @@
-import { Graphics, NineSlicePlane, Rectangle, Texture } from "pixi.js";
-import { IHitbox } from "../utils/IHitbox";
+import { Sprite, Texture, Graphics, Rectangle } from "pixi.js";
 import { PhysicsContainer } from "./PhysicsContainer";
+import { IHitbox } from "../utils/IHitbox";
 import { MapObjects } from "./MapObjects";
+import { GoodWitch } from "./GoodWitch";
 
 export class Platform extends PhysicsContainer implements IHitbox {
     private hitbox: Graphics;
-    private pot;
+    private pot: MapObjects | null;
+    private goodWitch: GoodWitch;
 
-    constructor() {
+    constructor(goodWitch: GoodWitch) {
         super();
-        const platform = new NineSlicePlane(Texture.from(
-            "platform1"), 15, 15, 15, 15);
+        this.goodWitch = goodWitch;
 
+        const platform = new Sprite(Texture.from("platform1"));
         this.addChild(platform);
-
 
         this.hitbox = new Graphics();
         this.hitbox.beginFill(0xFF00FF, 0.3);
@@ -21,28 +22,46 @@ export class Platform extends PhysicsContainer implements IHitbox {
         this.hitbox.endFill();
         this.addChild(this.hitbox);
 
-        if (Math.random() < 0.5) {
+        const rand = Math.random();
 
-            this.pot = new MapObjects("manaPot");
-            this.pot.position.x = platform.width / 2;
-            this.pot.position.y = platform.height - 60;
-
-            this.addChild(this.pot);
-        } else if (Math.random() < 0.33) {
-            this.pot = new MapObjects("healthPot");
-            this.pot.position.x = platform.width / 2;
-            this.pot.position.y = platform.height - 60;
-            this.addChild(this.pot);
-
-        } else if (Math.random() < 0.22) {
-            this.pot = new MapObjects("coin");
-            this.pot.position.x = platform.width / 2;
-            this.pot.position.y = platform.height - 60;
-            this.addChild(this.pot);
+        if (this.goodWitch.isManaFull() && !this.goodWitch.isHealthFull()) {
+            // Incrementar la probabilidad de healthPot
+            if (rand < 0.66) {
+                this.pot = new MapObjects("healthPot");
+            } else if (rand < 0.5) {
+                this.pot = new MapObjects("coin");
+            } else {
+                this.pot = null;
+            }
+        } else if (!this.goodWitch.isManaFull() && this.goodWitch.isHealthFull()) {
+            // Incrementar la probabilidad de manaPot
+            if (rand < 0.66) {
+                this.pot = new MapObjects("manaPot");
+            } else if (rand < 0.5) {
+                this.pot = new MapObjects("coin");
+            } else {
+                this.pot = null;
+            }
         } else {
-            this.pot = null;
+            // Distribución normal cuando ambos no están llenos
+            if (rand < 0.33) {
+                this.pot = new MapObjects("manaPot");
+            } else if (rand < 0.66) {
+                this.pot = new MapObjects("healthPot");
+            } else if (rand < 0.88) {
+                this.pot = new MapObjects("coin");
+            } else {
+                this.pot = null;
+            }
+        }
+
+        if (this.pot) {
+            this.pot.position.x = platform.width / 2;
+            this.pot.position.y = platform.height - 60;
+            this.addChild(this.pot);
         }
     }
+
     getHitbox(): Rectangle {
         return this.hitbox.getBounds();
     }
